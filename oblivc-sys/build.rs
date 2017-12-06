f88e200efc110090bd399e45de231a749a4b5a5e
Phillipp Schoppmann <schoppmann@informatik.hu-berlin.de>
2017-12-06 20:02:05 +0100

    Use bindgen to automatically generate bindings

--- oblivc-sys/build.rs
+++ oblivc-sys/build.rs
@@ -1,7 +1,9 @@
 extern crate walkdir;
+extern crate bindgen;
+
 
 use std::process::Command;
-use std::path::Path;
+use std::path::{Path,PathBuf};
 use std::env;
 use walkdir::WalkDir;
 
@@ -53,4 +55,26 @@ fn main() {
         }
     }
     println!("cargo:rerun-if-changed={}", oblivc_path.join("_build/libobliv.a").to_str().unwrap());
+
+    // add obliv-c source directory to CPATH
+    let mut paths = Vec::<PathBuf>::new();
+    if let Some(path) = env::var_os("CPATH") {
+        paths = env::split_paths(&path).collect();
+    }
+    paths.push(oblivc_path.join("src/ext/oblivc"));
+    let new_path = env::join_paths(paths).unwrap();
+    env::set_var("CPATH", &new_path);
+
+    // generate bindings
+    let bindings = bindgen::Builder::default()
+        .header(oblivc_path.join("src/ext/oblivc/obliv.h").to_str().unwrap())
+        .whitelisted_type("ProtocolDesc") // TODO: whitelist everything directly in obliv.h
+        .generate()
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
