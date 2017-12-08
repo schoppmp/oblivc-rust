280d77e7dea84b9d663dd5a6462dc68cd918eb02
Phillipp Schoppmann <schoppmann@informatik.hu-berlin.de>
2017-12-08 17:24:23 +0100

    Add bindings() for generating a pre-configured bindgen::Builder

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,43 @@
 extern crate libobliv_sys;
+#[macro_use]
+extern crate lazy_static;
+extern crate cc;
+extern crate bindgen;
 
-mod compiler;
-pub use compiler::*;
+use std::env;
+use std::path::PathBuf;
+
+lazy_static! {
+    pub static ref OBLIVC_ROOT : PathBuf = PathBuf::from(env!("DEP_OBLIV_ROOT"));
+    pub static ref OBLIVC_INCLUDE : Vec<PathBuf> =
+        env::split_paths(env!("DEP_OBLIV_INCLUDE")).collect();
+}
+
+
+pub fn compiler() -> cc::Build {
+    let mut builder = cc::Build::new();
+    OBLIVC_INCLUDE.iter().fold(
+        builder.compiler(OBLIVC_ROOT.join("bin/oblivcc")),
+        |builder, path| builder.include(path)
+    );
+    builder
+}
+
+pub fn bindings() -> bindgen::Builder {
+    bindgen::builder()
+        .clang_args(OBLIVC_INCLUDE.iter().map(|p| format!("-I{}", p.display())))
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn new_builder() {
-        let _ = compiler::new_builder();
+    fn new_compiler() {
+        let _ = compiler();
+    }
+    #[test]
+    fn new_bindings() {
+        let _ = bindings();
     }
 }
